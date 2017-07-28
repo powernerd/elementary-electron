6e20dbb80fe27c817f8f442fc8dbc5dabf82df94
@powernerd <powernerdsoftware@gmail.com>
2017-07-27 21:31:08 -0700

    elementary-electron #4
    Completed the required code changes for

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-require('cat-picture')
+//The first line should require our cat picture, and
+//also assign it to a variable:
+var picture = require('cat-picture')
+
+//We actually just want the source data for the picture, so get it, then
+//remove the image from the page.
+var src = picture.src
+picture.remove()
+
+/* Now install a visualization module for drawing polygons by typing npm
+install lightning-image-poly --save. Double check that your package.json has a new entry!
+Require the module by adding the line: */
+var image = require('lightning-image-poly')
+
+//Then render the image data we stored above by creating a new image
+//visualization.
+var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'})
+
+//If you run electron app.js you should see a cat, and be able to draw
+//polygons on it with the mouse!
